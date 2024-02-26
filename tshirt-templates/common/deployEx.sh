73b0d074f28e3b16d9d96ef68317425cb729b04c
psapna25 <104568771+psapna25@users.noreply.github.com>
2024-02-26 13:18:31 +0530

    Update deployEx.sh

--- tshirt-templates/common/deployEx.sh
+++ tshirt-templates/common/deployEx.sh
@@ -1,6 +1,8 @@
 echo "Applying conponent configuration to the cluster"
 kubectl create ns $serviceNamespace
 kubectl create ns $workloadNamespace
+kubectl label namespaces $serviceNamespace apps.tanzu.vmware.com/tap-ns=true
+kubectl label namespaces $workloadNamespace apps.tanzu.vmware.com/tap-ns=true
 
 kubectl apply -f ./rmqCluster.yaml
 
